a518bbd5dd79c449a36f5c5b843a1d2c25516ee8
MalachiRawson <rawsomal001@alpinesd.org>
2024-10-30 19:42:33 -0600

    it worked

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,10 @@
-#for Summation code here
+#for Summation code her
+EnteredNumber = int(input("Enter a number: "))
+CountingNumber = 1
+Sum = 0
+
+for i in range(EnteredNumber):
+    Sum = CountingNumber + Sum
+    CountingNumber = CountingNumber + 1
+
+print (str(Sum))
--- while_summation.py
+++ while_summation.py
@@ -1,1 +1,9 @@
 #while summation code following directions from Readme
+EnteredNumber = int (input ("Enter a number: "))
+CountingNumber = 1
+Sum = 0
+
+while CountingNumber <= EnteredNumber:
+    Sum = CountingNumber + Sum
+    CountingNumber = CountingNumber + 1
+print (str(Sum))
